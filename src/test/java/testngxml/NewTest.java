7dc7266c4e6df13ec4f2f58f6ba3bf863f565332
pragti gupta <pragtig6@gmail.com>
2024-08-14 11:59:53 +0530

    s

---
+++ src/test/java/testngxml/NewTest.java
@@ -0,0 +1,14 @@
+package testngxml;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void fTest() {
+	  
+	  Assert.assertEquals("hello", "hello"); 
+	  }
+  
+
+}
